8a53ae92aa0e21cfb6dc05f3c16c6f3b71a5c017
saidarao <saidarao.kotari@gmail.com>
2021-04-23 19:41:49 +0800

    rename Application

--- src/main/java/com/zuhike/coding/challenge/Application.java
+++ src/main/java/com/zuhike/coding/challenge/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZuhikeCodingChallengeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuhikeCodingChallengeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
