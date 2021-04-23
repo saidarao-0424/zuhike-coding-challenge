c0b7f4c7cb62690e77c03edf0510d2dc32e34b2c
saidarao <saidarao.kotari@gmail.com>
2021-04-23 19:54:31 +0800

    Adding logging

--- src/main/java/com/zuhike/coding/challenge/process/StoreOrderProcessor.java
+++ src/main/java/com/zuhike/coding/challenge/process/StoreOrderProcessor.java
@@ -2,12 +2,14 @@ package com.zuhike.coding.challenge.process;
 
 import com.zuhike.coding.challenge.model.StoreOrder;
 import com.zuhike.coding.challenge.model.StoreOrderDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@Slf4j
 public class StoreOrderProcessor implements ItemProcessor<StoreOrderDTO, StoreOrder> {
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.mm.yy");
@@ -15,6 +17,8 @@ public class StoreOrderProcessor implements ItemProcessor<StoreOrderDTO, StoreOr
     @Override
     public StoreOrder process(StoreOrderDTO storeOrder) throws Exception {
         //Validate the record for Validity
+        log.debug("Processing storeOrder ", storeOrder);
+
         return StoreOrder.builder().orderID(storeOrder.getOrderID())
                 .orderDate(LocalDate.parse(storeOrder.getOrderDate(), formatter))
                 .shipDate(LocalDate.parse(storeOrder.getShipDate(), formatter))
